777cd1cddd725d3cc5798d0f8e4ec3a01118534a
BlakeMScurr <blakemscurr@gmail.com>
2019-05-01 18:56:19 +1200

    Create handy divider program.

---
+++ example.go
@@ -0,0 +1,23 @@
+//Package main is an example package
+package main
+
+import (
+	"errors"
+	"fmt"
+)
+
+func main() {
+	err := PrintDivision(1, 2)
+	if err != nil {
+		panic(err.Error())
+	}
+}
+
+func PrintDivision(a, b int) (error) {
+	if b == 0 {
+		return errors.New("divide by zero error")
+	}
+	fmt.Println(a/b)
+	return nil
+}
+
